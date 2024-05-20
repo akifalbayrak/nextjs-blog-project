b03eca70176a7db51e9b65a21149cf7f2460057d
akif albayrak <akifalbayrak6161@gmail.com>
2024-05-20 15:54:15 +0300

    chore: Update home page to use lazy loading for images

--- pages/index.js
+++ pages/index.js
@@ -2,47 +2,23 @@ import { Fragment } from "react";
 
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
+import { getFeaturedPosts } from "../lib/posts-util";
 
-const DUMMY_POSTS = [
-    {
-        title: "Getting Started with Nextjs",
-        image: "getting-started-nextjs.png",
-        excerpt:
-            "Next.js is a popular and lightweight framework for static and server-rendered applications built with React.",
-        date: "2022-02-10",
-        slug: "getting-started-with-nextjs",
-    },
-    {
-        title: "Getting Started with Nextjs2",
-        image: "getting-started-nextjs.png",
-        excerpt:
-            "Next.js is a popular and lightweight framework for static and server-rendered applications built with React.",
-        date: "2022-02-10",
-        slug: "getting-started-with-nextjs",
-    },
-    {
-        title: "Getting Started with Nextjs3",
-        image: "getting-started-nextjs.png",
-        excerpt:
-            "Next.js is a popular and lightweight framework for static and server-rendered applications built with React.",
-        date: "2022-02-10",
-        slug: "getting-started-with-nextjs",
-    },
-    {
-        title: "Getting Started with Nextjs4",
-        image: "getting-started-nextjs.png",
-        excerpt:
-            "Next.js is a popular and lightweight framework for static and server-rendered applications built with React.",
-        date: "2022-02-10",
-        slug: "getting-started-with-nextjs",
-    },
-];
-
-export default function HomePage() {
+export default function HomePage(props) {
     return (
         <Fragment>
             <Hero />
-            <FeaturedPosts posts={DUMMY_POSTS} />
+            <FeaturedPosts posts={props.posts} />
         </Fragment>
     );
+}
+
+export async function getStaticProps() {
+    const featuredPosts = getFeaturedPosts();
+
+    return {
+        props: {
+            posts: featuredPosts,
+        },
+    };
 }
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,40 +1,16 @@
 import AllPosts from "../../components/posts/all-posts";
+import getAllPosts from "../../lib/posts-util";
 
-const DUMMY_POSTS = [
-    {
-        title: "Getting Started with Nextjs",
-        image: "getting-started-nextjs.png",
-        excerpt:
-            "Next.js is a popular and lightweight framework for static and server-rendered applications built with React.",
-        date: "2022-02-10",
-        slug: "getting-started-with-nextjs",
-    },
-    {
-        title: "Getting Started with Nextjs2",
-        image: "getting-started-nextjs.png",
-        excerpt:
-            "Next.js is a popular and lightweight framework for static and server-rendered applications built with React.",
-        date: "2022-02-10",
-        slug: "getting-started-with-nextjs",
-    },
-    {
-        title: "Getting Started with Nextjs3",
-        image: "getting-started-nextjs.png",
-        excerpt:
-            "Next.js is a popular and lightweight framework for static and server-rendered applications built with React.",
-        date: "2022-02-10",
-        slug: "getting-started-with-nextjs",
-    },
-    {
-        title: "Getting Started with Nextjs4",
-        image: "getting-started-nextjs.png",
-        excerpt:
-            "Next.js is a popular and lightweight framework for static and server-rendered applications built with React.",
-        date: "2022-02-10",
-        slug: "getting-started-with-nextjs",
-    },
-];
+export default function AllPostsPage(props) {
+    return <AllPosts posts={props.posts} />;
+}
 
-export default function AllPostsPage() {
-    return <AllPosts posts={DUMMY_POSTS} />;
+export function getStaticProps() {
+    const allPosts = getAllPosts();
+
+    return {
+        props: {
+            posts: allPosts,
+        },
+    };
 }
