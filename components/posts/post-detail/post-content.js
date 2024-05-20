f4c19c131f70bbb69028ef2fd3b03d1520f19dcf
akif albayrak <akifalbayrak6161@gmail.com>
2024-05-20 16:58:03 +0300

    chore: Update react-markdown dependency and add react-syntax-highlighter

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,5 +1,7 @@
 import PostHeader from "./post-header";
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import classes from "./post-content.module.css";
 import Image from "next/image";
@@ -39,6 +41,18 @@ export default function PostContent(props) {
             }
 
             return <p>{paragraph.children}</p>;
+        },
+
+        code(code) {
+            const { className, children } = code;
+            const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+            return (
+                <SyntaxHighlighter
+                    style={atomDark}
+                    language={language}
+                    children={children}
+                />
+            );
         },
     };
 
