aaf28f4d350dd23adcbe712ad407ca664888bcd6
akif albayrak <akifalbayrak6161@gmail.com>
2024-05-20 16:05:09 +0300

    chore: Update PostContent component to use dynamic post data

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,21 +3,15 @@ import ReactMarkdown from "react-markdown";
 
 import classes from "./post-content.module.css";
 
-const DUMMY_POSTS = {
-    title: "Getting Started with Nextjs",
-    image: "getting-started-nextjs.png",
-    date: "2022-02-10",
-    slug: "getting-started-with-nextjs",
-    content: "# This is a first post",
-};
+export default function PostContent(props) {
+    const { post } = props;
 
-export default function PostContent() {
-    const imagePath = `/images/posts/${DUMMY_POSTS.slug}/${DUMMY_POSTS.image}`;
+    const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
     return (
         <article className={classes.content}>
-            <PostHeader title={DUMMY_POSTS.title} image={imagePath} />
-            <ReactMarkdown>{DUMMY_POSTS.content}</ReactMarkdown>
+            <PostHeader title={post.title} image={imagePath} />
+            <ReactMarkdown>{post.content}</ReactMarkdown>
         </article>
     );
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,34 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData } from "../../lib/posts-util";
 
 export default function PostDetailPage(props) {
     return <PostContent post={props.post} />;
+}
+
+export function getStaticProps(context) {
+    const { params } = context;
+
+    const { slug } = params;
+
+    const postData = getPostData(slug);
+
+    return {
+        props: {
+            post: postData,
+        },
+        revalidate: 600,
+    };
+}
+
+export function getStaticPaths() {
+    const postFilenames = getPostFiles();
+
+    const slugs = postFilenames.map((fileName) =>
+        fileName.replace(/\.md$/, "")
+    );
+
+    return {
+        paths: slugs.map((slug) => ({ params: { slug } })),
+        fallback: false,
+    };
 }
