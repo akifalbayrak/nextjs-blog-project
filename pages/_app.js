4897245efb59f6293f6029106a322532d1428fd5
akif albayrak <akifalbayrak6161@gmail.com>
2024-05-21 13:33:46 +0300

    feat: Add meta tags to improve SEO and accessibility

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
+import Head from "next/head";
+
 import "../styles/globals.css";
 import Layout from "../components/layout/layout";
 
 function MyApp({ Component, pageProps }) {
     return (
         <Layout>
+            <Head>
+                <title>Events</title>
+                <meta name="description" content="Events" />
+                <meta
+                    name="viewport"
+                    content="initial-scale=1.0, width=device-width"
+                />
+            </Head>
             <Component {...pageProps} />
         </Layout>
     );
