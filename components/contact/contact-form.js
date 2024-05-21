7e3ce099aa27eb9dfa091691336ff055616e37f4
akif albayrak <akifalbayrak6161@gmail.com>
2024-05-21 13:20:31 +0300

    feat: Add form submission functionality to ContactForm component

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,22 +1,87 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import classes from "./contact-form.module.css";
+import Notification from "../ui/notification";
+
+async function sendContactData(contactDetails) {
+    const response = await fetch("/api/contact", {
+        method: "POST",
+        body: JSON.stringify(contactDetails),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.message || "Something went wrong!");
+    }
+}
+
 export default function ContactForm() {
     const emailInputRef = useRef();
     const nameInputRef = useRef();
     const messageInputRef = useRef();
-    function sendMessageHandler(event) {
+    const [requestStatus, setRequestStatus] = useState();
+    const [requestError, setRequestError] = useState();
+
+    useEffect(() => {
+        if (requestStatus === "success" || requestStatus === "error") {
+            const timer = setTimeout(() => {
+                setRequestStatus(null);
+                setRequestError(null);
+            }, 3000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [requestStatus]);
+
+    async function sendMessageHandler(event) {
         event.preventDefault();
-        fetch("/api/contact", {
-            method: "POST",
-            body: JSON.stringify({
+
+        setRequestStatus("pending");
+
+        try {
+            await sendContactData({
                 email: emailInputRef.current.value,
                 name: nameInputRef.current.value,
                 message: messageInputRef.current.value,
-            }),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
+            });
+            emailInputRef.current.value = "";
+            nameInputRef.current.value = "";
+            messageInputRef.current.value = "";
+        } catch (error) {
+            setRequestError(error.message);
+            setRequestStatus("error");
+        }
+
+        setRequestStatus("success");
+    }
+
+    let notification;
+
+    if (requestStatus === "pending") {
+        notification = {
+            status: "pending",
+            title: "Sending message...",
+            message: "Your message is on its way!",
+        };
+    }
+
+    if (requestStatus === "success") {
+        notification = {
+            status: "success",
+            title: "Success!",
+            message: "Message sent successfully!",
+        };
+    }
+
+    if (requestStatus === "error") {
+        notification = {
+            status: "error",
+            title: "Error!",
+            message: requestError,
+        };
     }
 
     return (
@@ -54,6 +119,13 @@ export default function ContactForm() {
                     <button>Send Message</button>
                 </div>
             </form>
+            {notification && (
+                <Notification
+                    status={notification.status}
+                    title={notification.title}
+                    message={notification.message}
+                />
+            )}
         </section>
     );
 }
