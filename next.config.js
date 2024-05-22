6bd29f96b2551264a7c462e6e9f7e071dc804f08
akif albayrak <akifalbayrak6161@gmail.com>
2024-05-22 21:45:31 +0300

    chore: Update react-syntax-highlighter and register new languages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
-module.exports = {
-    env: {
-        mongodb_username: "akif",
-        mongodb_password: "6w8qEvujS07THhr0",
-        mongodb_clustername: "cluster0",
-        mongodb_database: "my-blog",
-    },
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+    if (phase == PHASE_DEVELOPMENT_SERVER) {
+        return {
+            env: {
+                mongodb_username: "akif",
+                mongodb_password: "6w8qEvujS07THhr0",
+                mongodb_clustername: "cluster0",
+                mongodb_database: "my-blog-dev",
+            },
+        };
+    }
+    return {
+        env: {
+            mongodb_username: "akif",
+            mongodb_password: "6w8qEvujS07THhr0",
+            mongodb_clustername: "cluster0",
+            mongodb_database: "my-blog",
+        },
+    };
 };
